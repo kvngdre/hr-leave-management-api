5b4bb1e31b4c134ee4bba20ca6c4475f344eaeb7
kvngdre <kennedyugo2@gmail.com>
2024-05-16 11:44:23 +0100

    added put request change approval to leave request controller

--- HR.LeaveManagement.API/Controllers/LeaveRequestController.cs
+++ HR.LeaveManagement.API/Controllers/LeaveRequestController.cs
@@ -1,8 +1,10 @@
 using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Commands.CreateLeaveRequest;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Commands.UpdateLeaveRequest;
+using HR.LeaveManagement.Application.Features.LeaveRequests.Commands.UpdateLeaveRequest.ChangeLeaveRequestApproval;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Queries.GetLeaveRequestDetails;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Queries.GetLeaveRequestList;
+using HR.LeaveManagement.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -40,10 +42,18 @@ public class LeaveRequestController(IMediator mediator) : ControllerBase
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateLeaveType(int id, [FromBody] UpdateLeaveRequestDto dto)
+    public async Task<ActionResult<BaseCommandResponse>> UpdateLeaveType(int id, [FromBody] UpdateLeaveRequestDto dto)
     {
-        await _mediator.Send(new UpdateLeaveRequestCommand(id, dto.StartDate, dto.EndDate, dto.LeaveTypeId, dto.RequestComments, null));
+        var response = await _mediator.Send(new UpdateLeaveRequestCommand(id, dto.StartDate, dto.EndDate, dto.LeaveTypeId, dto.RequestComments));
 
-        return NoContent();
+        return response.Success ? Ok(response) : NotFound(response);
+    }
+
+    [HttpPut("change-approval/{id}")]
+    public async Task<ActionResult<BaseCommandResponse>> ChangeApproval(int id, [FromBody] ChangeLeaveRequestApprovalDto dto)
+    {
+        var response = await _mediator.Send(new ChangeLeaveRequestApprovalCommand(id, dto.Approved));
+
+        return response.Success ? Ok(response) : NotFound(response);
     }
 }
